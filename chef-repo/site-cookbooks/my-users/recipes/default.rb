698ef3805727a4e62cbbbfa27f3200723fe28bd0
yowcowvg <yoko_ohyama@voyagegroup.com>
2014-12-25 19:28:18 +0900

    my-users

---
+++ chef-repo/site-cookbooks/my-users/recipes/default.rb
@@ -0,0 +1,12 @@
+#
+# Cookbook Name:: my-users
+# Recipe:: default
+#
+# Copyright 2014, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+
+user 'vagrant' do
+  shell '/bin/zsh'
+end
