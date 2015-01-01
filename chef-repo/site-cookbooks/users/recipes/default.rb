3ce0c753bf5c88879818d98f17f339171518c1f1
yowcowvg <yoko_ohyama@voyagegroup.com>
2015-01-01 16:58:12 +0900

    fixed

--- chef-repo/site-cookbooks/users/recipes/default.rb
+++ chef-repo/site-cookbooks/users/recipes/default.rb
@@ -1,5 +1,5 @@
 #
-# Cookbook Name:: my-users
+# Cookbook Name:: users
 # Recipe:: default
 #
 # Copyright 2014, YOUR_COMPANY_NAME
@@ -9,4 +9,23 @@
 
 user 'vagrant' do
   shell '/bin/zsh'
+end
+
+bash 'install dotfiles' do
+  user 'vagrant'
+  group 'vagrant'
+  cwd '/home/vagrant'
+  code <<-EOC
+    cd /home/vagrant && git clone https://github.com/yowcow/dotfiles.git
+  EOC
+  creates '/home/vagrant/dotfiles'
+end
+
+bash 'install to vagrant' do
+  user 'vagrant'
+  group 'vagrant'
+  cwd '/home/vagrant'
+  code <<-EOC
+    cd /home/vagrant/dotfiles && make && make install
+  EOC
 end
